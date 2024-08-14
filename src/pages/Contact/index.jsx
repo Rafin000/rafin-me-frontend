1ec195fec56f5d66394ca549bb3d519231dfda75
Rafin000 <marufulislam00000@gmail.com>
2024-08-14 18:27:12 +0600

    Contact Page design done

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -8,20 +8,20 @@ function Contact() {
             <h1>Contact Me</h1>
             <p>If you have any questions, feel free to reach out to me through this form.</p>
             <div className="form-container">
-                <form>
-                <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" rows="5" required></textarea>
-                </div>
-                <button type="submit" className="send-button">Send</button>
+                <form className='form'>
+                    <div className="form-group">
+                        <label htmlFor="name" className='name'>Name</label>
+                        <input type="text" id="name" name="name" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email" className='name'>Email</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message" className='name'>Message</label>
+                        <textarea id="message" name="message" rows="5" required></textarea>
+                    </div>
+                    <button type="submit" className="send-button">Send</button>
                 </form>
             </div>
         </div>
