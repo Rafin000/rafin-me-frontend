d91230a787aa61ed09fe89bb9e1bc34e673cf8c2
Rafin000 <marufulislam00000@gmail.com>
2024-08-15 13:09:57 +0600

    removed admin from frontend

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Layout from './components/Layout';
 import Blog from './pages/Blog';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
-import Admin from './pages/Admin';
 
 function App() {
   return (
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog" element={<Blog />} />
-          <Route path="/admin" element={<Admin />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Layout>
